bb9a822354828bc98ad71c87c064facfd275405c
Francismar Condor <trollzer64@gmail.com>
2017-11-13 10:07:53 -0300

    Incluída a repetição e refinamento de detalhes.

--- main.c
+++ main.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main() {
-	unsigned int i, j;
-	unsigned int m=3, n=1, o=3, number;
+	unsigned long int i, j; //Variaveis para contagem//
+	unsigned long int m, n, o; //Variaveis para sequencia//
 
-	printf("Um numero positivo: ");
-	scanf("%u", &i);
-	setbuf(stdin, NULL);
+	do {
+		system("clear || cls");
+		printf("Insira um numero positivo: ");
+		scanf("%lu", &i);
+		setbuf(stdin, NULL);
 
-	j=0;
-	while (j < i) {
-		m=n;
-		n=o;
-		o=m+n;
-		j++;
-	}
-	if (((m-1)%i != 0)||(m == 1)) {
-		printf("Seu numero nao eh primo\n");
-	} else {
-		printf("Seu numero eh primo\n");
-	}
+		j = 0;
+		n = 1;
+		o = 3;
+		while (j < i) {
+			m=n;
+			n=o;
+			o=m+n;
+			j++;
+		}
+
+		if (((m-1)%i != 0)||(m == 1)) {
+			printf("Seu numero nao eh primo\n");
+		} else {
+			printf("Seu numero provavelmente eh primo\n");
+		}
+
+		printf("Continuar? (1=Sim/0=Nao)\n");
+		scanf("%lu", &j);
+		setbuf(stdin, NULL);
+
+	} while (j != 0);
+
 	printf("\n\n");
-	getchar();
 }
